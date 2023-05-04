baa345d141a4b5e47553f6746f124d6b516ff860
SwaroopKrishnaCS <panicker.swaroop@gmail.com>
2023-05-03 21:26:10 -0400

    Composition

--- src/compositionDemo/CarDelaershipMain.java
+++ src/compositionDemo/CarDelaershipMain.java
@@ -1,5 +1,7 @@
 package compositionDemo;
 
+import java.util.Scanner;
+
 public class CarDelaershipMain {
 
 	public static void main(String[] args) {
@@ -12,13 +14,23 @@ public class CarDelaershipMain {
 		
 		Car kiaSeltos = new Car("Kia", "Blue", 5, 20000);
 		
+		Car toyotaCorolla = new Car("Toyota", "Black", 5, 25000);
+		
 //		ds.addCarToDelaership(kiaSeltos);
 		
 //		ds.getCarName();
 		
 		ds.addCarsToDealership(hondaCivic);
 		ds.addCarsToDealership(kiaSeltos);
+		ds.addCarsToDealership(toyotaCorolla);
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Which car would yu like to buy? Honda, Kia or Toyota");
+		
+		String carRequested = sc.next();
+		
+		double priceOfRequestedCar = ds.getRequestedCarPrice(carRequested);
+		System.out.println("Price for the car: "+priceOfRequestedCar);
 
 	}
 
--- src/compositionDemo/Dealership.java
+++ src/compositionDemo/Dealership.java
@@ -5,8 +5,8 @@ public class Dealership {
 	public String dealershipName;
 
 	private Car[] carList = new Car[5];
-	
-	String [] nameList = new String[5];
+
+	String[] nameList = new String[5];
 
 //	public void addCarToDealership(Car carBroughtToDelership) {
 //		car = carBroughtToDelership;
@@ -15,14 +15,26 @@ public class Dealership {
 //	public void getCarName() {
 //		System.out.println(car.getBrandName());
 //	}
-	
+
 	public void addCarsToDealership(Car carBroughtToDelership) {
-		for(int i=0;i<carList.length;i++) {
-			if(carList[i]==null) {
-			carList[i]=carBroughtToDelership;
-			break;
+		for (int i = 0; i < carList.length; i++) {
+			if (carList[i] == null) {
+				carList[i] = carBroughtToDelership;
+				break;
 			}
 		}
+	}
+
+	public double getRequestedCarPrice(String carRequested) {
+		double priceOfRequestedCar = 0;
+		for (int i = 0; i < carList.length; i++) {
+			if (carList[i].getBrandName().equals(carRequested)) {
+				priceOfRequestedCar = carList[i].getPriceOfCar();
+				break;
+			}
+		}
+		return priceOfRequestedCar;
+
 	}
 
 }
