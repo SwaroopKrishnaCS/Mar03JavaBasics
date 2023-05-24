f18e75eb242e63f731ec861dd9b13401f0823ae7
SwaroopKrishnaCS <panicker.swaroop@gmail.com>
2023-05-23 21:21:10 -0400

    Static

---
+++ src/staticDemo/StaticExample.java
@@ -0,0 +1,17 @@
+package staticDemo;
+
+public class StaticExample extends ApplePhone{
+
+	public StaticExample(String memory, int ram, double price) {
+		super(memory, ram, price);
+		// TODO Auto-generated constructor stub
+	}
+	
+//	@Override
+//	public static double calculateDiscount() {
+//		
+//	}
+	
+	
+
+}
