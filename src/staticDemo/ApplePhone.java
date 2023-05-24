e486488c48f280fa2ab1c29618a2c8efcae2050b
SwaroopKrishnaCS <panicker.swaroop@gmail.com>
2023-05-23 21:21:58 -0400

    Static

--- src/staticDemo/ApplePhone.java
+++ src/staticDemo/ApplePhone.java
@@ -1,15 +1,25 @@
 package staticDemo;
 
 public class ApplePhone {
-	
+
 	public static String operatingSytem;
 	public String memory;
 	public int ram;
-	
-	public ApplePhone(String memory, int ram) {
+	public double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public ApplePhone(String memory, int ram, double price) {
 		super();
 		this.memory = memory;
 		this.ram = ram;
+		this.price = price;
 	}
 
 	public void setOperatingSytem(String operatingSytem1) {
@@ -19,7 +29,18 @@ public class ApplePhone {
 	public String getOperatingSytem() {
 		return operatingSytem;
 	}
-	
-	
+
+	public static double calculateDiscount() {
+		double discoutPerc = generateRandomNumber();
+		return discoutPerc / 100;
+	}
+
+	public static double generateRandomNumber() {
+		return Math.random() * 10;
+	}
+
+	public static double generateRandomNumber(int num1) {
+		return Math.random() * num1;
+	}
 
 }
--- src/staticDemo/MobileMain.java
+++ src/staticDemo/MobileMain.java
@@ -3,9 +3,9 @@ package staticDemo;
 public class MobileMain {
 
 	public static void main(String[] args) {
-		ApplePhone iphone12 = new ApplePhone("64GB", 6);
-		ApplePhone iphone13 = new ApplePhone("128GB", 8);
-		ApplePhone iphone14 = new ApplePhone("128GB", 12);
+		ApplePhone iphone12 = new ApplePhone("64GB", 6,999.99);
+		ApplePhone iphone13 = new ApplePhone("128GB", 8,1099.99);
+		ApplePhone iphone14 = new ApplePhone("128GB", 12,1199.99);
 
 		iphone12.setOperatingSytem("IOS");
 
@@ -16,10 +16,15 @@ public class MobileMain {
 		iphone12.setOperatingSytem("Android");
 
 		System.out.println(iphone12.getOperatingSytem());
+		
+//		iphone12.calculateDiscount();
 
 		System.out.println(iphone13.getOperatingSytem());
 		System.out.println(iphone14.getOperatingSytem());
+		
+		System.out.println(ApplePhone.operatingSytem);
 
+		ApplePhone.calculateDiscount();
 	}
 
 }
